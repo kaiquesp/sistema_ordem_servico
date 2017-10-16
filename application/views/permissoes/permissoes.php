087b9f6c4c010d25f3bb179b9b80a7616797d692
Kaique Alves <kaiqueexp@gmail.com>
2017-10-16 08:33:47 -0200

    Visualizar permissões

--- application/views/permissoes/permissoes.php
+++ application/views/permissoes/permissoes.php
@@ -1,118 +1,75 @@
-<a href="<?php echo base_url();?>index.php/permissoes/adicionar" class="btn btn-success"><i class="icon-plus icon-white"></i> Adicionar Permissão</a>
-<?php
-if(!$results){?>
-
-        <div class="widget-box">
-        <div class="widget-title">
-            <span class="icon">
-                <i class="icon-lock"></i>
-            </span>
-            <h5>Permissões</h5>
-
-        </div>
-
-        <div class="widget-content nopadding">
-            <table class="table table-bordered">
-                <thead>
-                    <tr>
-                        <th>#</th>
-                        <th>Nome</th>
-                        <th>Data de Criação</th>
-                        <th>Situação</th>
-                        <th></th>
-                    </tr>
-                </thead>
-                <tbody>
-                    <tr>
-                        <td colspan="5">Nenhuma Permissão foi cadastrada</td>
-                    </tr>
-                </tbody>
-            </table>
-        </div>
+  <div class="">
+    <div class="page-title">
+      <div class="title_left">
+        <h3>Permissões</h3>
+      </div>
     </div>
 
-<?php }else{
-	
-
-?>
-<div class="widget-box">
-     <div class="widget-title">
-        <span class="icon">
-            <i class="icon-lock"></i>
-         </span>
-        <h5>Permissões</h5>
-
-     </div>
-
-<div class="widget-content nopadding">
-
-
-<table class="table table-bordered ">
-    <thead>
-        <tr>
-            <th>#</th>
-            <th>Nome</th>
-            <th>Data de Criação</th>
-            <th>Situação</th>
-            <th></th>
-        </tr>
-    </thead>
-    <tbody>
-        <?php foreach ($results as $r) {
+    <div class="clearfix"></div>
+    <div class="col-md-12 col-sm-12 col-xs-12">
+      <div class="x_panel">
+            <a href="<?php echo base_url();?>permissoes/adicionar" class="btn btn-success"><i class="icon-plus icon-white"></i> Adicionar Permissão</a>
+        <?php if(isset($msg)){
+          echo "<div class='box-header with-border'>".$msg."</div>";
+        } 
+        ?>
+        <?php if ($this->session->flashdata('error') == TRUE): ?>
+          <p><?php echo $this->session->flashdata('error'); ?></p>
+        <?php endif; ?>
+        <?php if ($this->session->flashdata('success') == TRUE): ?>
+          <p><?php echo $this->session->flashdata('success'); ?></p>
+        <?php endif; ?>
+        <div class="x_content">
+        <?php
+        if(!$results){?>
+          <table id="datatable-responsive" class="table table-striped table-bordered dt-responsive nowrap" cellspacing="0" width="100%">
+            <thead>
+              <tr>
+                <th>#</th>
+                <th>Nome</th>
+                <th>CNPJ</th>
+                <th>CPF</th>
+                <th>Telefone</th>
+                <th>Ação</th>
+              </tr>
+            </thead>
+            <tbody>
+              <td colspan="5">Nenhuma permissão encontrada!</td>
+            </tbody>
+          </table>
+		<?php }else{?>
+			<table id="datatable-responsive" class="table table-striped table-bordered dt-responsive nowrap" cellspacing="0" width="100%">
+            <thead>
+              <tr>
+                <th>#</th>
+                <th>Nome</th>
+                <th>Data de criacao</th>
+                <th>Situacão</th>
+                <th>Ação</th>
+              </tr>
+            </thead>
+            <tbody>
+              <?php foreach ($results as $r) {
             if($r->situacao == 1){$situacao = 'Ativo';}else{$situacao = 'Inativo';}
             echo '<tr>';
             echo '<td>'.$r->idPermissao.'</td>';
             echo '<td>'.$r->nome.'</td>';
             echo '<td>'.date('d/m/Y',strtotime($r->data)).'</td>';
             echo '<td>'.$situacao.'</td>';
-            echo '<td>
-                      <a href="'.base_url().'index.php/permissoes/editar/'.$r->idPermissao.'" class="btn btn-info tip-top" title="Editar Permissão"><i class="icon-pencil icon-white"></i></a>
-                      <a href="#modal-excluir" role="button" data-toggle="modal" permissao="'.$r->idPermissao.'" class="btn btn-danger tip-top" title="Desativar Permissão"><i class="icon-remove icon-white"></i></a>
-                  </td>';
+            echo '<td><a href="'.base_url().'index.php/permissoes/editar/'.$r->idPermissao.'" class="btn btn-info tip-top" title="Editar Permissão"><i class="fa fa-edit"></i></a>
+                      <a href="#modal-excluir" role="button" data-toggle="modal" permissao="'.$r->idPermissao.'" class="btn btn-danger tip-top" title="Desativar Permissão"><i class="fa fa-trash-o"></i></a></td>';
             echo '</tr>';
         }?>
         <tr>
             
         </tr>
-    </tbody>
-</table>
-</div>
-</div>
-<?php echo $this->pagination->create_links();}?>
 
+            </tbody>
+          </table>
+		<?php }?>
 
-
- 
-<!-- Modal -->
-<div id="modal-excluir" class="modal hide fade" tabindex="-1" role="dialog" aria-labelledby="myModalLabel" aria-hidden="true">
-  <form action="<?php echo base_url() ?>index.php/permissoes/desativar" method="post" >
-  <div class="modal-header">
-    <button type="button" class="close" data-dismiss="modal" aria-hidden="true">×</button>
-    <h5 id="myModalLabel">Desativar Permissão</h5>
+        </div>
+      </div>
+    </div>
   </div>
-  <div class="modal-body">
-    <input type="hidden" id="idPermissao" name="id" value="" />
-    <h5 style="text-align: center">Deseja realmente desativar esta permissão?</h5>
-  </div>
-  <div class="modal-footer">
-    <button class="btn" data-dismiss="modal" aria-hidden="true">Cancelar</button>
-    <button class="btn btn-danger">Excluir</button>
-  </div>
-  </form>
 </div>
-
-
-<script type="text/javascript">
-$(document).ready(function(){
-
-
-   $(document).on('click', 'a', function(event) {
-        
-        var permissao = $(this).attr('permissao');
-        $('#idPermissao').val(permissao);
-
-    });
-
-});
-
-</script>
