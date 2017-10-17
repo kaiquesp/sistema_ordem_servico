dbf5da571a46326e8a2286e33b5f8519e9f9d86b
Kaique Alves <kaiqueexp@gmail.com>
2017-10-17 11:13:42 -0200

    Ajuste Exclusão de produtos

--- application/views/produtos/produtos.php
+++ application/views/produtos/produtos.php
@@ -59,14 +59,10 @@
                             <div class="modal-content">
                               <div class="modal-header">
                                 <button type="button" class="close" data-dismiss="modal" aria-label="Fechar"><span aria-hidden="true">&times;</span></button>
-                                <h4 class="modal-title" id="modalLabel">Excluir Item</h4>
+                                <h4 class="modal-title" id="modalLabel">Excluir Produto</h4>
                               </div>
                               <div class="modal-body">
                                 <form action="<?php echo base_url() ?>produtos/excluir" method="post" >
-                                  <div class="modal-header">
-                                    <button type="button" class="close" data-dismiss="modal" aria-hidden="true">×</button>
-                                    <h5 id="myModalLabel">Excluir Produto</h5>
-                                  </div>
                                   <div class="modal-body">
                                     <input type="hidden" id="idProduto" name="id" value="<?php echo $r->idProdutos; ?>" />
                                     <h5 style="text-align: center">Deseja realmente excluir este produto?</h5>
@@ -76,10 +72,6 @@
                                     <button class="btn btn-danger">Excluir</button>
                                   </div>
                                   </form>
-                              </div>
-                              <div class="modal-footer">
-                                <button type="button" class="btn btn-primary">Sim</button>
-                         <button type="button" class="btn btn-default" data-dismiss="modal">N&atilde;o</button>
                               </div>
                             </div>
                           </div>
