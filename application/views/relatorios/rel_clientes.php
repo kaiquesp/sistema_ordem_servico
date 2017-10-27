3b25e84728704643b2305d1d4e1f0264e691193b
Kaique Alves <kaiqueexp@gmail.com>
2017-10-27 08:51:10 -0200

    Relatórios Clientes

--- application/views/relatorios/rel_clientes.php
+++ application/views/relatorios/rel_clientes.php
@@ -7,22 +7,8 @@
                       <h2>Relatórios Rápidos</h2>
                       <div class="clearfix"></div>
                     </div>     
-                    <?php if ($this->session->flashdata('error') == TRUE): ?>
-                      <div class="alert alert-success alert-dismissible fade in" role="alert">
-                        <button type="button" class="close" data-dismiss="alert" aria-label="Close"><span aria-hidden="true">×</span>
-                        </button>
-                        <p><?php echo $this->session->flashdata('error'); ?></p>
-                      </div>
-                    <?php endif; ?>
-                    <?php if ($this->session->flashdata('success') == TRUE): ?>
-                      <div class="alert alert-success alert-dismissible fade in" role="alert">
-                        <button type="button" class="close" data-dismiss="alert" aria-label="Close"><span aria-hidden="true">×</span>
-                        </button>
-                        <p><?php echo $this->session->flashdata('success'); ?></p>
-                      </div>
-                    <?php endif; ?>
                     <div class="x_content">
-                        <div class="col-md-offset-3 col-sm-offset-3 col-xs-offset-3">
+                        <div align="center">
                             <a href="<?php echo base_url()?>relatorios/clientesRapid" class="btn btn-app col-centered"  target="_blank">
                                 <i class="fa fa-user"></i> Todos os clientes
                             </a>
@@ -37,38 +23,25 @@
                       <h2>Relatórios Customizáveis</h2>
                       <div class="clearfix"></div>
                     </div>    
-                    <?php if ($this->session->flashdata('error') == TRUE): ?>
-                      <div class="alert alert-success alert-dismissible fade in" role="alert">
-                        <button type="button" class="close" data-dismiss="alert" aria-label="Close"><span aria-hidden="true">×</span>
-                        </button>
-                        <p><?php echo $this->session->flashdata('error'); ?></p>
-                      </div>
-                    <?php endif; ?>
-                    <?php if ($this->session->flashdata('success') == TRUE): ?>
-                      <div class="alert alert-success alert-dismissible fade in" role="alert">
-                        <button type="button" class="close" data-dismiss="alert" aria-label="Close"><span aria-hidden="true">×</span>
-                        </button>
-                        <p><?php echo $this->session->flashdata('success'); ?></p>
-                      </div>
-                    <?php endif; ?>
                     <div class="x_content">
                         <div class="row">
-                            <form target="_blank" action="<?php echo base_url()?>relatorios/clientesCustom" method="get">
-                                <div class="well well-lg">
-                                    <div class="col-md-4 col-xs-12">
-                                        <label for="">Cadastrado de:</label>
-                                        <input type="date" name="dataInicial" class="form-control" />
-                                    </div>
-                                    <div class="col-md-4 col-xs-12">
-                                        <label for="">até:</label>
-                                        <input type="date" name="dataFinal" class="form-control" />
-                                    </div>
-                                    <div class="col-md-4 col-xs-12">
-                                        <br>
-                                        <button class="btn btn-dark"><i class="fa fa-print"></i> Imprimir</button>
-                                    </div>
-                                </div>
-                            </form>
+                        	<div class="well well-lg">
+                                <form target="_blank" action="<?php echo base_url()?>relatorios/clientesCustom" method="get">
+                                    
+                                        <div class="col-md-4 col-xs-12">
+                                            <label for="">Cadastrado de:</label>
+                                            <input type="date" name="dataInicial" class="form-control" />
+                                        </div>
+                                        <div class="col-md-4 col-xs-12">
+                                            <label for="">até:</label>
+                                            <input type="date" name="dataFinal" class="form-control" />
+                                        </div>
+                                        <div class="col-md-4 col-xs-12">
+                                            <br>
+                                            <button class="btn btn-dark"><i class="fa fa-print"></i> Imprimir</button>
+                                        </div>
+                                </form>
+                            </div>
                         </div>
                     </div>
                 </div>
